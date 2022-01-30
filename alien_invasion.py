ac34d24d0cf4d8e1742ff888e04a1192e9d57680
Okalash <kalachnikovsasha@gmail.com>
2022-01-30 13:14:03 +0200

    ship movement and react for keyboard

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,29 +3,48 @@ import pygame
 from settings import Settings
 from ship import Ship
 
-class AlienInvasion:  # class to resource and behaviour of game
 
+# class to resource and behaviour of game
+class AlienInvasion:
+
+    # init game and create resource
     def __init__(self):
-        pygame.init()  # init game and create resource
+        pygame.init()
         self.settings = Settings()  # init settings
         self.screen = pygame.display.set_mode((self.settings.screen_width,
                                                self.settings.screen_height))  # set display size (surface)
         pygame.display.set_caption('Alien Invasion')
         self.ship = Ship(self)
 
-    def run_game(self):  # start game
+    #  start game
+    def run_game(self):
         while True:
-            for event in pygame.event.get():  # wait event from keyboard or mouse (event manager)
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
+            self._update_screen()
+            self.ship.update()
 
-            self.screen.fill(self.settings.bg_color)  # new color by settings
-            self.ship.blitme()
-            pygame.display.flip()  # draw last screen (update field of fire)
+    # wait react for mouse or keyboard
+    def _check_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:  # quit from game
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = True
+                if event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                if event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
+    # redraw screen
+    def _update_screen(self):
+        self.screen.fill(self.settings.bg_color)  # new color by settings
+        self.ship.blitme()
+        pygame.display.flip()  # draw last screen (update field of fire)
 
 
 if __name__ == '__main__':  # run if file called directly
     ai = AlienInvasion()
     ai.run_game()
-
-
--- ship.py
+++ ship.py
@@ -1,16 +1,28 @@
 import pygame
 
 
+# class for control Ship
 class Ship:
 
-    def __init__(self, ai_game):  # ai_game is instance AlienInvasion
+    # ai_game is instance AlienInvasion
+    def __init__(self, ai_game):
         self.screen = ai_game.screen  # load game screen
         self.screen_rect = ai_game.screen.get_rect()  # load game screen rectangle (for right location)
 
         self.image = pygame.image.load('images/ship.bmp')  # ship picture load
         self.rect = self.image.get_rect()  # ship rectangle (as size)
         self.rect.midbottom = self.screen_rect.midbottom  # every ship load from bottom center
+        # run indicators
+        self.moving_right = False
+        self.moving_left = False
 
+    # draw ship in current location
     def blitme(self):
-        self.screen.blit(self.image, self.rect)  # draw ship in current location
+        self.screen.blit(self.image, self.rect)
 
+    # update current location based on run indicator
+    def update(self):
+        if self.moving_right:
+            self.rect.x += 1
+        if self.moving_left:
+            self.rect.x -= 1
